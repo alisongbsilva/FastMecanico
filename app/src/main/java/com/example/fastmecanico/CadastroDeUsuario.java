7e6b6a15eb544b77383ac2762dcfbaee825f780b
BrunoSantos666 <51796953+BrunoSantos666@users.noreply.github.com>
2019-06-15 23:47:35 -0300

    telas
    tela cadastro e veiculo e suas respectivas classes

---
+++ app/src/main/java/com/example/fastmecanico/CadastroDeUsuario.java
@@ -0,0 +1,13 @@
+package com.example.fastmecanico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroDeUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_de_usuario);
+    }
+}
---
+++ app/src/main/java/com/example/fastmecanico/CadastroVeiculo.java
@@ -0,0 +1,13 @@
+package com.example.fastmecanico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroVeiculo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_veiculo);
+    }
+}
