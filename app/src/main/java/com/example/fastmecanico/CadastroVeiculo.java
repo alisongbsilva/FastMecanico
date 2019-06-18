5d325e3de5e1344bb668cf9f8677d2fb8518e3dc
BrunoSantos666 <51796953+BrunoSantos666@users.noreply.github.com>
2019-06-18 16:25:03 -0300

    Telas completas
    alteradas as tlas e linkadas as telas de cadastro e usuario a demandas
    @alisongbsilva

--- app/src/main/java/com/example/fastmecanico/CadastroVeiculo.java
+++ app/src/main/java/com/example/fastmecanico/CadastroVeiculo.java
@@ -2,6 +2,8 @@ package com.example.fastmecanico;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CadastroVeiculo extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class CadastroVeiculo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_veiculo);
+
+        Button concluido = (Button) findViewById(R.id.concluido);
+        concluido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v ){
+                setContentView(R.layout.activity_tela_usuario);
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/fastmecanico/TelaUsuario.java
+++ app/src/main/java/com/example/fastmecanico/TelaUsuario.java
@@ -2,6 +2,8 @@ package com.example.fastmecanico;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TelaUsuario extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class TelaUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_usuario);
+
+        Button demandas = (Button) findViewById(R.id.demandas);
+        demandas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v ){
+                setContentView(R.layout.telademanda);
+            }
+        });
+
     }
 }
